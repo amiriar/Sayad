b11f00aadf9d63160010b34f6ace9d16f5442a35
amiriar <amiriar.irr@gmail.com>
2023-10-27 15:16:29 +0330

    some SEO shit

--- app/page.js
+++ app/page.js
@@ -5,6 +5,13 @@ import HomePage from '@/components/HomePage'
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>هنرستان صیاد شیرازی</title>
+        {/* <meta name="google" content="nositelinkssearchbox" key="sitelinks" /> */}
+        <meta name="google" content="notranslate" key="notranslate" />
+        <meta name="robots" content="index,follow" />
+      </Head>
+
       <HomePage/>
       {/* <about/> */}
       {/* <workers/> */}
